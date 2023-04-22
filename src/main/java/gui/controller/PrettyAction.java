bb2859f2d41f29d980b5ad6b04ee45ca87493619
Djomla-n <mladendimitrijevic40@gmail.com>
2023-04-23 01:52:52 +0200

    -dodato da pretty dugme ne spusta u novi red kada se napise as or ili in

--- src/main/java/gui/controller/PrettyAction.java
+++ src/main/java/gui/controller/PrettyAction.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 public class PrettyAction extends AbstractMaturskiAction{
     private static String[] nizReciVelika;
     private static String[] nizReciNorm;
-    private static String[] nizFunkcija={"SELECT", "FROM", "WHERE", "UPDATE", "INSERT INTO"};
+    private static String[] nizFunkcija={"SELECT", "FROM", "WHERE", "UPDATE", "INSERT INTO","AS","OR","IN"};
     private static String tekstPanel;
     private String tekstPane;
     private MyFrame instance;
@@ -23,12 +23,20 @@ public class PrettyAction extends AbstractMaturskiAction{
 
         for (int i = 0; i < nizReciVelika.length; i++) {
             for (int j = 0; j < nizFunkcija.length; j++) {
-                if(nizReciVelika[i].equals(nizFunkcija[j])){
-                    if(i==0) {
-                        nizReciNorm[i] =  nizReciVelika[i];
+                if (nizReciVelika[i].equals(nizFunkcija[j])) {
+                    if (nizReciVelika[i].equals("AS") || nizReciVelika[i].equals("OR") || nizReciVelika[i].equals("IN")) {
+                        if (i == 0) {
+                            nizReciNorm[i] = nizReciVelika[i];
+                        } else {
+                            nizReciNorm[i] = nizReciVelika[i];
+                        }
                     }
                     else {
-                        nizReciNorm[i] = "\n" + nizReciVelika[i];
+                        if (i == 0) {
+                            nizReciNorm[i] = nizReciVelika[i];
+                        } else {
+                            nizReciNorm[i] = "\n"+nizReciVelika[i];
+                        }
                     }
                 }
             }
